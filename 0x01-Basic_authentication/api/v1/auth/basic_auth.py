90ff8d52d7abcc82034e44cf7c3bf5ab61808365
Bayero-abdul <bayeroa65@gmail.com>
2023-11-12 06:34:08 +0100

    Return the decoded value of a Base64 string base64_authorization_header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 """
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -21,3 +22,19 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split()[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ Returns the decoded value of a Base64 string
+        base64_authorization_header
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            base64_auth_header = base64.b64decode(base64_authorization_header)
+        except Exception:
+            return None
+
+        return base64_auth_header.decode()
