5f4e43f0db5a5dd94c43f3f3e53223308a1f0cf1
Bayero-abdul <bayeroa65@gmail.com>
2023-11-15 11:16:44 +0100

    Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,6 +4,7 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
@@ -17,7 +18,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -42,3 +43,17 @@ class DB:
         self._session.commit()
 
         return new_user
+
+    def find_user_by(self, **kwds):
+        """ Filters and returns a user found by the input arguments
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwds).first()
+
+            if user is None:
+                raise NoResultFound("No user found")
+
+            return user
+
+        except InvalidRequestError as e:
+            raise e
