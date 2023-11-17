bec0bc8e21a21cb83eb508a122e0d353739ae395
Bayero-abdul <bayeroa65@gmail.com>
2023-11-17 03:16:42 +0100

    Get session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -57,3 +57,16 @@ class Auth:
             return False
 
         return False
+
+    def create_session(self, email: str) -> str:
+        """find the user corresponding to the email, generate a new
+        UUID and store it in the database as the user’s session_id
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                session_id = _generate_uuid()
+                setattr(user, 'session_id', session_id)
+                return session_id
+        except NoResultFound:
+            return None
