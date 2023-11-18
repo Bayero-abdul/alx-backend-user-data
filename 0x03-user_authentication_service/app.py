b1d1b834bc08058f8ae562db4893b624b053ad5e
Bayero-abdul <bayeroa65@gmail.com>
2023-11-18 08:43:53 +0100

    Fix code

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -87,7 +87,7 @@ def get_reset_password_token():
     return jsonify({"email": f"{email}", "reset_token": f"{reset_token}"}), 200
 
 
-@app.route('/reset_password' methods=['PUT'], strict_slashes=False)
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password():
     """Update password
     """
